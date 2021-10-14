aaef0c07322d765e92ba0228c989022ebb5e7196
wndud0159 <wndud0159@naver.com>
2021-10-14 20:03:07 +0900

    first commit

---
+++ ch03/webpack.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    entry: {
+        app: "./main.js",
+    },
+    module: {},
+    plugins: [],
+    output: {
+        fileName: "[name].js", // app.js
+        path: "./dist",
+    },
+}
